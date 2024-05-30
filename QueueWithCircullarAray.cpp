62e732c517ac3e09e74b0a9cd5444a9d64eb08d8
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-30 15:04:22 +0700

    membuat decicion making else untuk ngecek bila antrian tidak kosong

--- QueueWithCircullarAray.cpp
+++ QueueWithCircullarAray.cpp
@@ -28,8 +28,14 @@ class queues {
             if (FRONT == -1) {
                 FRONT = 0;
                 REAR = 0;
-            };
-        };
-                 
+             }
+            else {
+                // jika REAR berada di posisi terakhir array,kembali ke awal array
+                if (REAR == max -1)
+                    REAR = 0;
+                else 
+                    REAR = REAR + 1;
+            }
+        };     
 };
     
